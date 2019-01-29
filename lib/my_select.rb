cc009e0e968990a69c898093ba766d94fabd0f54
Njenga Kariuki <njengak@Njengas-MacBook-Pro-2.local>
2019-01-29 13:25:44 -0800

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  i = 0
+  select_arr = []
+  while i < array.length
+  select_arr.push(array[i]) if yield array[i]
+  i += 1
+  end
+  select_arr
 end
